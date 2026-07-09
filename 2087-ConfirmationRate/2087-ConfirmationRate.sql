9944fb24994bf90abb051e310131d8f21b87d13b
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:11:06 +0530

    Create 2087-ConfirmationRate

---
+++ 2087-ConfirmationRate/2087-ConfirmationRate.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/9/2026, 3:11:05 PM
+SELECT
+    s.user_id,
+    ROUND(
+        IFNULL(SUM(c.action = 'confirmed') / COUNT(c.action), 0),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
