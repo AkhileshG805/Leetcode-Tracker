01c0e864fcd66572e654000473222d1f3adf0840
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:11:45 +0530

    Create 740-DeleteAndEarn

---
+++ 740-DeleteAndEarn/740-DeleteAndEarn.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:11:45 PM
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int inc = 0;
+        int exc = 0;
+        
+        int count[] = new int[10001];
+        for(int val: nums) count[val]++;
+        
+        for(int i = 0; i <= 10000; i++) {
+            int ni = exc + count[i] * i;
+            int ne = Math.max(inc, exc);
+            
+            inc = ni;
+            exc = ne;
+        }
+        return Math.max(inc, exc);
+    }
+}
