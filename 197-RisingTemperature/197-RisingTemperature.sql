2e7f04d4c1710508a68bb0b5e6a3b2742d603a0f
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:12:46 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,6 @@
+-- Last updated: 7/9/2026, 3:12:46 PM
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature > W2.temperature;
