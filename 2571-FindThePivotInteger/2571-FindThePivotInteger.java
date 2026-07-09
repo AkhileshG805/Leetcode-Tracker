d4ae798a2d30b280dd757674285bacfb04d765e4
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:11:00 +0530

    Create 2571-FindThePivotInteger

---
+++ 2571-FindThePivotInteger/2571-FindThePivotInteger.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 3:11:00 PM
+class Solution {
+    public int pivotInteger(int n) {
+        int total=(n*(n+1))/2;
+        int s=(int)Math.sqrt(total);
+        if((s*s==total)) return s;
+        else return -1;   }
+    }
