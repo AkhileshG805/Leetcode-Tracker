2b9cedfe69be1e1277e8ae81632a162133c37d4f
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:12:06 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 3:12:05 PM
+class Solution {
+    public int arrangeCoins(int n) {
+        int row=1;
+        while(n>=row){
+            n=n-row;
+            row++;
+        }
+        return row-1;
+        
+    }
+}
