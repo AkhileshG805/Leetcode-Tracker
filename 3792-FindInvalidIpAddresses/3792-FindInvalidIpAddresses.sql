c6f7ae6fc85d2f3ea7f0b6f9e5819e38529c5517
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:10:33 +0530

    Create 3792-FindInvalidIpAddresses

---
+++ 3792-FindInvalidIpAddresses/3792-FindInvalidIpAddresses.sql
@@ -0,0 +1,20 @@
+-- Last updated: 7/9/2026, 3:10:33 PM
+SELECT
+    ip,
+    COUNT(*) AS invalid_count
+FROM Logs
+WHERE
+    LENGTH(ip) - LENGTH(REPLACE(ip, '.', '')) != 3
+
+    OR SUBSTRING_INDEX(ip, '.', 1) REGEXP '^0[0-9]'
+    OR SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) REGEXP '^0[0-9]'
+    OR SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) REGEXP '^0[0-9]'
+    OR SUBSTRING_INDEX(ip, '.', -1) REGEXP '^0[0-9]'
+
+    OR SUBSTRING_INDEX(ip, '.', 1) > 255
+    OR SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) > 255
+    OR SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) > 255
+    OR SUBSTRING_INDEX(ip, '.', -1) > 255
+
+GROUP BY ip
+ORDER BY invalid_count DESC, ip DESC;
