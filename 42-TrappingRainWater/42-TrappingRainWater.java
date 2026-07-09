1223c028462b3b36513a9acffcd2c240b598985d
AkhileshG805 <akhileshgirish029@gmail.com>
2026-07-09 15:14:13 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:14:12 PM
+class Solution {
+    public int trap(int[] h) {
+        int l = 0, r = h.length - 1;
+        int lm = 0, rm = 0, ans = 0;
+        while (l < r) {
+            if (h[l] < h[r]) {
+                lm = Math.max(lm, h[l]);
+                ans += lm - h[l++];
+            } else {
+                rm = Math.max(rm, h[r]);
+                ans += rm - h[r--];
+            }
+        }
+        return ans;
+    }
+}
