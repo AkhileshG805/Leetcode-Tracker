994481749b052b6d202004aea8bcc04e07125ef4
AkhileshG805 <akhileshgirish029@gmail.com>
2026-08-31 16:22:39 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/version/java/14-LongestCommonPrefix_20260831_162236.java
@@ -0,0 +1,14 @@
+// Last updated: 8/31/2026, 4:22:36 PM
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        int j=1;
+4        for(int i=1;i<nums.length;i++){
+5            if(nums[i]!=nums[i-1]){
+6                nums[j]=nums[i];
+7                j++;
+8            }
+9        }
+10        return j;
+11    }
+12}
+13
