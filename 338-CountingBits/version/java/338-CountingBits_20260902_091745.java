7b49cb206cd311a2d9a22548dd61e74a0deef3a7
AkhileshG805 <akhileshgirish029@gmail.com>
2026-09-02 09:17:48 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/version/java/338-CountingBits_20260902_091745.java
@@ -0,0 +1,9 @@
+// Last updated: 9/2/2026, 9:17:45 AM
+1class Solution {
+2    public int[] countBits(int num) {
+3        int[] f = new int[num + 1];
+4        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+5        return f;
+6        
+7    }
+8}
